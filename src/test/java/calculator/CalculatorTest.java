ac08ab7a8a90fde4ad2ccd1d01301301ccead515
etd93 <etd93@naver.com>
2025-10-19 23:53:51 +0900

    test(Calculator): 기본 구분자(, :) 합산 테스트 추가

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -4,6 +4,7 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import calculator.domain.Calculator;
 
@@ -12,8 +13,15 @@ public class CalculatorTest extends NsTest {
     Calculator calculator = new Calculator();
 
     @Test
+    @DisplayName("빈 문자열일 경우 0을 반환한다.")
     void Empty_String_ReturnsZero() {
         assertThat(calculator.calculate("0")).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("기본 구분자(, :) : 합산 테스트 ")
+    void Default_Separator_Calculate() {
+        assertThat(calculator.calculate("1,2:3")).isEqualTo(6);
     }
 
 
