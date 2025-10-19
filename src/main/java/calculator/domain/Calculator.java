bbb4780d45f4d10813fa2470431392247aada5e5
etd93 <etd93@naver.com>
2025-10-20 05:39:48 +0900

    refactor: 사용자 입력을 기본 구분자로 구분하여 숫자 추출(Calculator) -> Delimiter로 책임 분리

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -1,5 +1,7 @@
 package calculator.domain;
 
+import java.util.List;
+
 public class Calculator {
     public Calculator() {}
 
@@ -8,9 +10,10 @@ public class Calculator {
             return 0;
         }
 
-        String[] tokens = inputString.split("[,:]");
+        Delimiter delimiter = Delimiter.ofDefault();
+        List<String> inputNumbers = delimiter.split(inputString);
         int sum = 0;
-        for (String token : tokens) {
+        for (String token : inputNumbers) {
             sum += Integer.parseInt(token);
         }
 
---
+++ src/main/java/calculator/domain/Delimiter.java
@@ -0,0 +1,44 @@
+package calculator.domain;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Delimiter {
+
+    private static final String DEFAULT_COMMA = ",";
+    private static final String DEFAULT_COLON = ":";
+
+    private final Set<String> tokens;
+    private final Pattern splitPattern;
+
+    private Delimiter(Set<String> tokens) {
+        this.tokens = Collections.unmodifiableSet(new LinkedHashSet<>(tokens));
+        this.splitPattern = Pattern.compile(makingUnionRegex(tokens));
+    }
+
+    public static Delimiter ofDefault() {
+        return new Delimiter(new LinkedHashSet<>(List.of(DEFAULT_COLON, DEFAULT_COMMA)));
+    }
+
+    public static Delimiter currentDelimiters(Collection<String> tokens) {
+        return new Delimiter(new LinkedHashSet<>(tokens));
+    }
+
+    public List<String> split(String input) {
+        return Arrays.asList(splitPattern.split(input));
+    }
+
+
+    private static String makingUnionRegex(Set<String> tokens) {
+        return tokens.stream()
+                .map(Pattern::quote)
+                .collect(Collectors.joining("|"));
+    }
+
+}
