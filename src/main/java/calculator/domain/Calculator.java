26e921bc85503abe1463ec6c3d92a82fc7a618b9
etd93 <etd93@naver.com>
2025-10-19 23:48:32 +0900

    feat(Calculator): 빈 문자열 입력 시 0 반환 추가

---
+++ src/main/java/calculator/domain/Calculator.java
@@ -0,0 +1,12 @@
+package calculator.domain;
+
+public class Calculator {
+    public Calculator() {}
+
+    public int calculate(String inputString) {
+        if(inputString.isEmpty()) {
+            return 0;
+        }
+        return 0;
+    }
+}
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -5,12 +5,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import calculator.domain.Calculator;
 
 public class CalculatorTest extends NsTest {
 
+    Calculator calculator = new Calculator();
+
     @Test
     void Empty_String_ReturnsZero() {
-        assertThat(Calculator.calculate("0")).isEqualTo("0");
+        assertThat(calculator.calculate("0")).isEqualTo(0);
     }
 
 
