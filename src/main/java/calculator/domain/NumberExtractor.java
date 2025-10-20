0ca6eb5cd0fd601a6773562f6876619dd8f1de8e
etd93 <etd93@naver.com>
2025-10-20 21:55:13 +0900

    feat(NumberExtractor): 숫자 사이에 공백이 존재하면 예외 발생 구현

--- src/main/java/calculator/domain/NumberExtractor.java
+++ src/main/java/calculator/domain/NumberExtractor.java
@@ -9,11 +9,17 @@ public class NumberExtractor {
         List<PositiveNumber> numbers = new ArrayList<>();
 
         for (String token : tokenizedNumbers) {
+            validate(token);
             numbers.add(new PositiveNumber(token));
         }
         return numbers;
     }
 
+    public void validate(String token) {
+        if (token.contains(" ")) {
+            throw new IllegalArgumentException("ERROR: 숫자 사이에 공백이 존재할 수 없습니다.");
+        }
+    }
 
 
 }
