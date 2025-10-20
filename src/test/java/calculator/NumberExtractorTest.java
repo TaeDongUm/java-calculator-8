c9ed46c104ade0c0008433da047be30f11d4aadb
etd93 <etd93@naver.com>
2025-10-20 21:52:50 +0900

    test(NumberExtractorTest): 구분자로 분리된 토큰에 공백이 있으면 예외 발생 테스트 추가

--- src/test/java/calculator/NumberExtractorTest.java
+++ src/test/java/calculator/NumberExtractorTest.java
@@ -2,6 +2,7 @@ package calculator;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import calculator.domain.Delimiter;
 import calculator.domain.NumberExtractor;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class NumberExtractorTest extends NsTest {
 
@@ -17,9 +19,9 @@ public class NumberExtractorTest extends NsTest {
 
     private static Stream<Arguments> provideListTokensForCalculation() {
         return Stream.of(
-                Arguments.of(List.of("1", "-2","3")),
-                Arguments.of(List.of("1","1.4","5")),
-                Arguments.of(List.of("1","a","3"))
+                Arguments.of(List.of("1", "-2", "3")),
+                Arguments.of(List.of("1", "1.4", "5")),
+                Arguments.of(List.of("1", "a", "3"))
         );
     }
 
@@ -29,6 +31,18 @@ public class NumberExtractorTest extends NsTest {
     void When_Input_Contains_Invalid_Tokens(List<String> input) {
         assertThatThrownBy(() -> extractor.extractNumber(input))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("숫자 사이에 공백이 포함되면 예외가 발생한다.")
+    @ValueSource(strings = {"1 ,2", " 3", "4 ", "5, 6", "7 :8", "9: 10"})
+    void When_Tokens_Contains_Whitespace(String input) {
+        Delimiter delimiter = Delimiter.ofDefault();
+        List<String> tokens = delimiter.split(input);
+        NumberExtractor extractor = new NumberExtractor();
+
+        assertThatThrownBy(() ->
+                extractor.extractNumber(tokens)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Override
