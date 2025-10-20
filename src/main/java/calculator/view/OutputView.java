ada6f68702b9916728241c2bc231e64442422ba5
etd93 <etd93@naver.com>
2025-10-20 20:36:25 +0900

    refactor: 구분자로 구분된 수는 양수이므로 양수로 변경, 에러 메시지 출력

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -2,8 +2,12 @@ package calculator.view;
 
 public class OutputView {
 
-    public void output(String result) {
+    public void output(int result) {
         System.out.print("결과 : ");
         System.out.println(result);
+    }
+
+    public void printError(String error) {
+        System.out.println(error);
     }
 }
