8f8503bde6dee600a2c06b441e261af721d192b9
etd93 <etd93@naver.com>
2025-10-20 20:58:29 +0900

    feat(NumberExtractor): 구분자 분리 후 빈 토큰이거나 숫자가 아니면 예외 발생 구현

---
+++ src/main/java/calculator/domain/NumberExtractor.java
@@ -0,0 +1,35 @@
+package calculator.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberExtractor {
+
+    public List<Integer> extractNumber(List<String> tokenizedNumbers) {
+        List<Integer> numbers = new ArrayList<>();
+
+        for (String token : tokenizedNumbers) {
+            validate(token);
+            numbers.add(Integer.parseInt(token));
+        }
+        return numbers;
+    }
+
+    public void validate(String token) {
+        validateIfTokenEmpty(token);
+        validateIfTokensAreOtherThanNumber(token);
+    }
+
+    public void validateIfTokenEmpty(String token) {
+        if (token.isEmpty()) {
+            throw new IllegalArgumentException("ERROR: 구분자 사이에 빈 문자열은 존재할 수 없습니다.");
+        }
+    }
+
+    public void validateIfTokensAreOtherThanNumber(String token) {
+        if (!token.matches("\\d+")) {
+            throw new IllegalArgumentException("ERROR: 0이상의 정수만 가능합니다.");
+        }
+    }
+
+}
