d06465e43befeb118aa8457666642f0c55daf531
etd93 <etd93@naver.com>
2025-10-20 21:11:42 +0900

    feat(calculator): numberExtractor, delimiterExtractor 의존성 주입 및 계산 로직 구현

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -3,18 +3,25 @@ package calculator.domain;
 import java.util.List;
 
 public class Calculator {
-    public Calculator() {}
+
+    private final NumberExtractor numberExtractor;
+    private final DelimiterExtractor delimiterExtractor;
+
+    public Calculator(NumberExtractor numberExtractor, DelimiterExtractor delimiterExtractor) {
+        this.numberExtractor = numberExtractor;
+        this.delimiterExtractor = delimiterExtractor;
+    }
 
     public int calculate(String inputString) {
-        if(inputString.isEmpty()) {
+        if (inputString == null || inputString.isEmpty()) {
             return 0;
         }
-
-        Delimiter delimiter = Delimiter.ofDefault().withCustom('e'); // 커스텀 구분자 추출 기능 추후 추가 필요
-        List<String> inputNumbers = delimiter.split(inputString);
+        Delimiter delimiter = Delimiter.ofDefault().withCustom(delimiterExtractor.extractDelimiter(inputString));
+        List<String> tokenizedNumbers = delimiter.split(inputString);
+        List<Integer> positiveNumbers = numberExtractor.extractNumber(tokenizedNumbers);
         int sum = 0;
-        for (String token : inputNumbers) {
-            sum += Integer.parseInt(token);
+        for (Integer token : positiveNumbers) {
+            sum += token;
         }
 
         return sum;
