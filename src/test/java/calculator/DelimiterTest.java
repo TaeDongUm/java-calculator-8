1815109b1eb0b1391f95d368f14f5722892b67ca
etd93 <etd93@naver.com>
2025-10-20 15:19:40 +0900

    test(DelimiterTest): 정규식 메타문자 구분자 허용하는지 체크 테스트

--- src/test/java/calculator/DelimiterTest.java
+++ src/test/java/calculator/DelimiterTest.java
@@ -1,12 +1,16 @@
 package calculator;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import calculator.domain.Delimiter;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class DelimiterTest extends NsTest {
 
@@ -18,8 +22,18 @@ public class DelimiterTest extends NsTest {
         assertDoesNotThrow(() ->
                 delimiter.withCustom('e')
         );
+    }
 
+    @ParameterizedTest
+    @ValueSource(chars = {'*', '.', '?', '+', '|', '^', '(', ')', '[', ']', '{', '}', '\\'})
+    @DisplayName("정규식 메타문자 구분자 허용 테스트")
+    void Custom_Delimiter_Contains_Regex_Meta_Char_Test(char regexMetaChar) {
+        Delimiter delimiter = Delimiter.ofDefault().withCustom(regexMetaChar);
 
+        String input = "1" + regexMetaChar + "2" + regexMetaChar + "3";
+        List<String> result = delimiter.split(input);
+
+        assertThat(result).contains("1", "2", "3");
     }
 
     @Override
