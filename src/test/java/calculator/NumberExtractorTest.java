5e39f91d45698f5be2f64950c353bcce4e08323e
etd93 <etd93@naver.com>
2025-10-20 20:44:01 +0900

    refactor(NumberExtractorTest): NumberExtractor.extractNumber의 매개변수가 List<String>로 수정되어서 테스트 케이스도 수정

--- src/test/java/calculator/NumberExtractorTest.java
+++ src/test/java/calculator/NumberExtractorTest.java
@@ -4,18 +4,29 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import calculator.domain.NumberExtractor;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class NumberExtractorTest extends NsTest {
 
     NumberExtractor extractor = new NumberExtractor();
 
+    private static Stream<Arguments> provideListTokensForCalculation() {
+        return Stream.of(
+                Arguments.of(List.of("1", "-2","3")),
+                Arguments.of(List.of("1","1.4","5")),
+                Arguments.of(List.of("1","a","3"))
+        );
+    }
+
     @ParameterizedTest
     @DisplayName("음수, 소수, 문자가 포함된 경우 IllegalArgumentException 발생")
-    @ValueSource(strings = {"1,-2,3", "1,2.5,3", "1,a,3"})
-    void When_Input_Contains_Invalid_Tokens(String input) {
+    @MethodSource("provideListTokensForCalculation")
+    void When_Input_Contains_Invalid_Tokens(List<String> input) {
         assertThatThrownBy(() -> extractor.extractNumber(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
