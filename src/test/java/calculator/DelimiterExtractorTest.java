08f00d90f067ec9a531967ef1e4d271fd6d7edaf
etd93 <etd93@naver.com>
2025-10-20 14:42:38 +0900

    refactor(DelimiterExtractorTest): 람다 표현식 메서드 체이닝 수정

--- src/test/java/calculator/DelimiterExtractorTest.java
+++ src/test/java/calculator/DelimiterExtractorTest.java
@@ -2,6 +2,7 @@ package calculator;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import calculator.domain.DelimiterExtractor;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,10 +15,8 @@ public class DelimiterExtractorTest extends NsTest {
     @DisplayName("커스텀 구분자 길이가 2글자 이상일 경우 예외 발생")
     void If_Custom_Delimiter_Length_Over_One() {
         assertThatThrownBy (() ->
-                delimiterExtractor.validateIfCustomDelimiterLengthOverOne("//++\\n1,2;3")
-                        .isInstanceOf(IllegalArgumentException.class)
-
-        );
+                delimiterExtractor.validateIfCustomDelimiterLengthOverOne("//++\\n1,2;3"))
+                .isInstanceOf(IllegalArgumentException.class);
 
     }
 
@@ -25,30 +24,27 @@ public class DelimiterExtractorTest extends NsTest {
     @DisplayName("커스텀 구분자가 개행 문자일 경우 예외 발생")
     void If_Custom_Delimiter_EqualTo_Newline_Character() {
         assertThatThrownBy (() ->
-                delimiterExtractor.validateIfCustomDelimiterHasNewLine("//\n\\n1,2;3")
-                        .isInstanceOf(IllegalArgumentException.class)
+                delimiterExtractor.validateIfCustomDelimiterHasNewLine("//\n\\n1,2;3"))
+                .isInstanceOf(IllegalArgumentException.class);
 
-        );
+
     }
 
     @Test
     @DisplayName("커스텀 구분자가 공백일 경우 예외 발생")
     void If_Custom_Delimiter_Has_Whitespace_Character() {
         assertThatThrownBy (() ->
-                delimiterExtractor.validateIfCustomDelimiterHasWhitespace("// \\n1,2;3")
-                        .isInstanceOf(IllegalArgumentException.class)
-
-        );
+                delimiterExtractor.validateIfCustomDelimiterHasWhitespace("// \\n1,2;3"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("커스텀 구분자로 숫자가 들어오는 경우 예외 발생")
     void If_Custom_Delimiter_Has_Number_Character() {
         assertThatThrownBy (() ->
-                delimiterExtractor.validateIfCustomDelimiterHasNumber("//3\\n1,2;3")
-                        .isInstanceOf(IllegalArgumentException.class)
+                delimiterExtractor.validateIfCustomDelimiterHasNumber("//3\\n1,2;3"))
+                .isInstanceOf(IllegalArgumentException.class);
 
-        );
     }
 
     @Override
